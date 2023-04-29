7bbc9bb9552ee641d6dea085ffc8d5f2a7cb465f
Williamatthewood <w.wood@tcu.edu>
2023-04-29 11:00:29 -0500

    wrote questions and set up inquirerer logic

---
+++ index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    'Enter up to 3 characters for your logo.',
+    'What color should the text be? Enter a color or hexadecimal number.',
+    'What shape should your logo be?',
+    'What should the shape be? Enter a color or hexadecimal number.',
+]
+
+function init(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'logoCharacters',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: questions[1],
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: questions[2],
+                choices: [
+                    "Circle",
+                    "Triangle",
+                    "Square"
+                ]
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: questions[3],
+            }
+        ])
+        .then((answers) => {
+            generateLogo("./examples/logo.svg", answers);
+        })
+}
+
+function generateLogo(filePath, data) {
+    //code needs to:
+    // destructure the responses
+    // check the shape to determine which class to generate
+    // verify the input is no longer than 3 characters
+    //      Maybe use max-length package?
+    // writeFile with the passed filePath and file name
+
+    console.log('Generated logo.svg');
+}
+
+init();
