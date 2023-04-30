972b1c8aec73fa8057bb0a97ec472e164757e10d
Williamatthewood <w.wood@tcu.edu>
2023-04-29 21:14:36 -0500

    main logic written. Circle successfully generates

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
 
 const questions = [
     'Enter up to 3 characters for your logo.',
@@ -13,7 +16,7 @@ function init(){
         .prompt([
             {
                 type: 'input',
-                name: 'logoCharacters',
+                name: 'text',
                 message: questions[0],
             },
             {
@@ -33,25 +36,39 @@ function init(){
             },
             {
                 type: 'input',
-                name: 'shapeColor',
+                name: 'fillColor',
                 message: questions[3],
             }
         ])
         .then((answers) => {
-            console.log(answers);
-            // generateLogo("./examples/logo.svg", answers);
+            generateLogo("./examples/logo.svg", answers);
         })
 }
 
 function generateLogo(filePath, data) {
     //code needs to:
     // destructure the responses
+    let { text, textColor, shape, fillColor } = data;
+
     // check the shape to determine which class to generate
+    switch (shape) {
+        case 'Circle':
+            shape = new Circle(text, textColor, fillColor);
+            break;
+        case 'Triangle':
+            console.log('Triangle was chosen');
+            break;
+        case 'Square':
+            console.log('Square was chosen');
+            break;
+        default:
+            console.log('No valid shape was provided');
+            break;
+    }
     // verify the input is no longer than 3 characters
     //      Maybe use max-length package?
     // writeFile with the passed filePath and file name
-
-    console.log('Generated logo.svg');
+    fs.writeFile(filePath, shape.render(), (err) => err ? console.error(err) : console.log('Generated logo.svg'));
 }
 
 init();
